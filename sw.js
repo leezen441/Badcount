1d327a47644f8c13b1a0dafdb265cbda2dbb316a
leezen441 <leezen441@gmail.com>
2026-05-22 18:20:55 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // - Cache เบาๆ + network-first strategy
 // ============================================================
 
-const CACHE_NAME = "badcount-v8";
+const CACHE_NAME = "badcount-v9";
 const PRECACHE_URLS = [
   "/",
   "/index.html",
